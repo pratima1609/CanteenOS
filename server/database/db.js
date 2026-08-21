906df1b91ce00a9e9b69597d810e4d823a429013
pratima1609 <vpratima057@gmail.com>
2026-08-22 02:38:06 +0530

    Prepare CanteenOS backend for cloud deployment

--- server/database/db.js
+++ server/database/db.js
@@ -1,16 +1,23 @@
 const mysql = require('mysql2/promise');
+
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'canteen_os',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {
+    minVersion: 'TLSv1.2',
+    // rejectUnauthorized: false
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
-
 // Verify connection on startup
 pool.getConnection()
   .then(conn => {
